f57c3767d9e3d805f3c050f8aadadcce1364dc8c
psw2020 <psw2020@gmail.com>
2021-03-04 17:07:06 +0300

    1

--- src/preload/index.js
+++ src/preload/index.js
@@ -21,9 +21,14 @@ ipcRenderer.on('taskList', (_, data) => {
 function clearObj(arr) {
     let a = [];
     arr.forEach(v => {
-        let {DATE_START: date, DOCUMENT_OUT_HEADER_ID: doh, MARK: mark, MODEL: model, REGNO: regno} = v;
+        let {DATE_START: date, DOCUMENT_OUT_HEADER_ID: doh, MARK: mark, MODEL: model, REGNO: regno, CLIENT_ID:clid, CLIENT_CONTACT_ID:contact} = v;
         let dateFormat = DateTime.fromISO(date).toFormat('dd.LL HH:mm');
-        a.push({date: dateFormat, doh, mark, model, regno});
+        a.push({date: dateFormat, doh, mark, model, regno, clid, contact});
     });
     return a;
 }
+
+window.loadBaseInfo = (doh, clid, contact) =>{
+    console.log(doh,clid,contact);
+}
+
--- src/renderer/javascripts/index.js
+++ src/renderer/javascripts/index.js
@@ -7,14 +7,21 @@ window.onload = () => {
 
 window.createTaskList = (arr) => {
     let str = '';
+
     arr.forEach(v => {
         str += `
-          <div class="taskListItem">
+          <div class="taskListItem" data-doh="${v.doh}" data-clid="${v.clid}" data-contact="${v.contact}">
             <p class="date">${v.date}</p>
             <p class="model">${v.mark} ${v.model}</p>
             <p class="gosnumber">${v.regno}</p>
         </div>
   `;
-        document.getElementsByClassName('taskList')[0].innerHTML = str;
     });
+
+    document.getElementsByClassName('taskList')[0].innerHTML = str;
+    let items = document.querySelectorAll('.taskListItem');
+    items.forEach(v => v.addEventListener('click', () => {
+        console.log(v.dataset.doh, v.dataset.clid, v.dataset.contact);
+    }))
 }
+
