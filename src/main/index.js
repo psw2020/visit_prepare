4720a92ee4bb6bbf47d1178ff57d2944a57c20ae
psw2020 <psw2020@gmail.com>
2021-03-16 13:52:24 +0300

    v 0.1

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 import VisitPrepare from './application';
 import {checkForUpdates} from './updater'
-
 new VisitPrepare();
+
 setTimeout(() => {
     checkForUpdates()
 }, 2000)
--- src/main/updater.js
+++ src/main/updater.js
@@ -1,6 +1,8 @@
 import {autoUpdater} from 'electron-updater';
+import path from "path";
 
 autoUpdater.logger = require("electron-log")
+autoUpdater.logger.transports.file.resolvePath = () => path.join(process.env.APPDATA, 'VisitPrepare/logs/main.log');
 autoUpdater.logger.transports.file.level = "info"
 
 export const checkForUpdates = ()=>{
